7acf506b417655e885d308350dc748fcdc0f20d7
Michael Kiersten <156722337+python-datascience-analysis@users.noreply.github.com>
2024-06-15 00:41:17 +0100

    Add files via upload

---
+++ cli/script.js
@@ -0,0 +1,17 @@
+
+const container = document.querySelector("#container");
+
+for (i=0;i<256;i++){
+const div = document.createElement("div");
+container.appendChild(div);
+}
+
+div.addEventListener("hover", ){
+    
+}
+
+
+
+
+
+
