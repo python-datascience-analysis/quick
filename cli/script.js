d420be862fa2d398a8792dd53f4d7748eab30116
Michael Kiersten <156722337+python-datascience-analysis@users.noreply.github.com>
2024-06-27 05:22:43 +0100

    Update script.js

--- cli/script.js
+++ cli/script.js
@@ -1,14 +1,15 @@
 
+
 const container = document.querySelector("#container");
 
 for (i=0;i<256;i++){
 const div = document.createElement("div");
 container.appendChild(div);
+    
+div.addEventListener("mouseover", function(){
+    div.style.backgroundColor = "black"})
 }
 
-div.addEventListener("hover", ){
-    
-}
 
 
 
